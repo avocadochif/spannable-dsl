89897dc6fa1a9bda0be2297cd3676309f93ddc43
stepan revytskyi <stepanrevytskyi@nerdzlab.com>
2022-03-07 21:44:00 +0200

    Added BufferType to set text method.

--- library/src/main/java/com/avocadochif/spannabledsl/library/extensions/TextViewExtensions.kt
+++ library/src/main/java/com/avocadochif/spannabledsl/library/extensions/TextViewExtensions.kt
@@ -4,5 +4,8 @@ import android.widget.TextView
 import com.avocadochif.spannabledsl.library.builders.SpannableBuilder
 
 fun TextView.spannable(init: SpannableBuilder.() -> Unit) {
-    text = SpannableBuilder(this.context).apply(init).build().toSpannableString()
+    setText(
+        SpannableBuilder(this.context).apply(init).build().toSpannableString(),
+        TextView.BufferType.SPANNABLE
+    )
 }
