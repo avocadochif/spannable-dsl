a15d8600539cf25d28951f390fa43b1918dffac7
avocadochif <avocadochif@gmail.com>
2022-03-09 21:51:45 +0200

    Refactor.

--- library/src/main/java/com/avocadochif/spannabledsl/library/models/decoration/Decoration.kt
+++ library/src/main/java/com/avocadochif/spannabledsl/library/models/decoration/Decoration.kt
@@ -2,7 +2,7 @@ package com.avocadochif.spannabledsl.library.models.decoration
 
 import com.avocadochif.spannabledsl.library.enums.TextDecorationType
 
-sealed class Decoration()
+sealed class Decoration
 
 data class TextDecoration(
     val type: TextDecorationType
